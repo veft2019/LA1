ca09156558112ec4ca90577586c1e0d7512d09ca
IvarKristinn <hallssonivarkristinn@gmail.com>
2019-09-05 10:45:27 +0000

    Data populated

--- TechnicalRadiation.Repositories/Data/NewsItemDataProvider.cs
+++ TechnicalRadiation.Repositories/Data/NewsItemDataProvider.cs
@@ -20,7 +20,257 @@ namespace TechnicalRadiation.Repositories.Data
                 DateCreated = DateTime.Now,
                 DateModified = DateTime.Now,
                 ModifiedBy = _adminName
-            }
+            },
+            new NewsItem {
+                Id = 2,
+                Title = "Title of the news article 2",
+                ImgSource = "https://example.com/news-item/2.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 3,
+                Title = "Title of the news article 3",
+                ImgSource = "https://example.com/news-item/3.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 4,
+                Title = "Title of the news article 4",
+                ImgSource = "https://example.com/news-item/4.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 5,
+                Title = "Title of the news article 5",
+                ImgSource = "https://example.com/news-item/5.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 6,
+                Title = "Title of the news article 6",
+                ImgSource = "https://example.com/news-item/6.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 7,
+                Title = "Title of the news article 7",
+                ImgSource = "https://example.com/news-item/7.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 8,
+                Title = "Title of the news article 8",
+                ImgSource = "https://example.com/news-item/8.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 9,
+                Title = "Title of the news article 9",
+                ImgSource = "https://example.com/news-item/9.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 10,
+                Title = "Title of the news article 10",
+                ImgSource = "https://example.com/news-item/10.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 11,
+                Title = "Title of the news article 11",
+                ImgSource = "https://example.com/news-item/11.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 12,
+                Title = "Title of the news article 12",
+                ImgSource = "https://example.com/news-item/12.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 13,
+                Title = "Title of the news article 13",
+                ImgSource = "https://example.com/news-item/13.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 14,
+                Title = "Title of the news article 14",
+                ImgSource = "https://example.com/news-item/14.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 15,
+                Title = "Title of the news article 15",
+                ImgSource = "https://example.com/news-item/15.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 16,
+                Title = "Title of the news article 16",
+                ImgSource = "https://example.com/news-item/16.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 17,
+                Title = "Title of the news article 17",
+                ImgSource = "https://example.com/news-item/17.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 18,
+                Title = "Title of the news article 18",
+                ImgSource = "https://example.com/news-item/18.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 19,
+                Title = "Title of the news article 19",
+                ImgSource = "https://example.com/news-item/19.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 20,
+                Title = "Title of the news article 20",
+                ImgSource = "https://example.com/news-item/20.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 21,
+                Title = "Title of the news article 21",
+                ImgSource = "https://example.com/news-item/21.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 22,
+                Title = "Title of the news article 22",
+                ImgSource = "https://example.com/news-item/22.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 23,
+                Title = "Title of the news article 23",
+                ImgSource = "https://example.com/news-item/23.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 24,
+                Title = "Title of the news article 24",
+                ImgSource = "https://example.com/news-item/24.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 25,
+                Title = "Title of the news article 25",
+                ImgSource = "https://example.com/news-item/25.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new NewsItem {
+                Id = 26,
+                Title = "Title of the news article 26",
+                ImgSource = "https://example.com/news-item/26.jpg",
+                ShortDescription = "A short description",
+                LongDescription = "A long description",
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+                ModifiedBy = _adminName
+            },
         };
     }
 }
