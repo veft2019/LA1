b0f0f4002d00fc83fdb408a9abb429abb17346f9
Hjörtur Jóhann Vignisson <hjorturv17@ru.is>
2019-09-05 15:59:37 +0000

    stuff

--- TechnicalRadiation.Models/Dtos/NewsItemDetailDto.cs
+++ TechnicalRadiation.Models/Dtos/NewsItemDetailDto.cs
@@ -2,6 +2,11 @@ namespace TechnicalRadiation.Models.Dtos
 {
     public class NewsItemDetailDto
     {
-        
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ImgSource { get; set; }
+        public string ShortDescription { get; set; } 
+        public string  LongDescription { get; set; }
+        public DateTime PublishDate { get; set; }
     }
 }
--- TechnicalRadiation.Models/Dtos/NewsItemDto.cs
+++ TechnicalRadiation.Models/Dtos/NewsItemDto.cs
@@ -5,8 +5,6 @@ namespace TechnicalRadiation.Models.Dtos
         public int Id { get; set; }
         public string Title { get; set; }
         public string ImgSource { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; } 
-        
+        public string ShortDescription { get; set; }        
     }
 }
