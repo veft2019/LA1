8a1ab74a30d1920f2ba760d9df2147627e3e0776
Hjörtur Jóhann Vignisson <hjorturv17@ru.is>
2019-09-05 18:15:54 +0000

    nothing

--- TechnicalRadiation.Models/Dtos/NewsItemDto.cs
+++ TechnicalRadiation.Models/Dtos/NewsItemDto.cs
@@ -1,6 +1,6 @@
 namespace TechnicalRadiation.Models.Dtos
 {
-    public class NewsItemDto
+    public class NewsItemDto : HyperMediaModel
     {   
         public int Id { get; set; }
         public string Title { get; set; }
