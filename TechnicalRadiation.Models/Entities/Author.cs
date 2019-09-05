65c18d3720ab0e9f1608b638d119a252f4703d6d
Hjörtur Jóhann Vignisson <hjorturv17@ru.is>
2019-09-05 16:06:26 +0000

    Entity models finished

--- TechnicalRadiation.Models/Entities/Author.cs
+++ TechnicalRadiation.Models/Entities/Author.cs
@@ -2,6 +2,14 @@ namespace TechnicalRadiation.Models.Entities
 {
     public class Author
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ProfileImageSource { get; set; }
+        public string Bio { get; set; }
+
+        // Metadata - Database specific properties
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public string ModifiedBy { get; set; }
     }
 }
