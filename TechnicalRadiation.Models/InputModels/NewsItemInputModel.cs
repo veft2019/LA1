4b25880fb4db498b5445e54d4e9b4006dbdb4148
Hjörtur Jóhann Vignisson <hjorturv17@ru.is>
2019-09-05 17:50:14 +0000

    Annotations on newsitem entity sem gugga var buin ad gera

--- TechnicalRadiation.Models/InputModels/NewsItemInputModel.cs
+++ TechnicalRadiation.Models/InputModels/NewsItemInputModel.cs
@@ -1,10 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace TechnicalRadiation.Models.InputModels
 {
     public class NewsItemInputModel
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
+        [Url]
         public string ImgSource { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string ShortDescription { get; set; }
-        public string LongDescription { get; set; } 
+        [MinLength(50)]
+        [MaxLength(255)]
+        public string LongDescription { get; set; }
+        [Required]
+        public DateTime PublishDate { get; set; }
     }
 }
